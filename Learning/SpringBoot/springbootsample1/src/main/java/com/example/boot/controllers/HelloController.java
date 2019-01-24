4a735c7f6e1663950b5712f46fe9fe90edddd6bf
TridentAmit <amit.codechef@gmail.com>
2019-01-25 00:28:37 +0530

    Docker connection between mongo and springbott

--- Learning/SpringBoot/springbootsample1/src/main/java/com/example/boot/controllers/HelloController.java
+++ Learning/SpringBoot/springbootsample1/src/main/java/com/example/boot/controllers/HelloController.java
@@ -4,12 +4,19 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.boot.model.Customer;
+import com.example.boot.mongo.dao.CustomerRepository;
+
 @RestController
 public class HelloController {
+	
+	@Autowired
+	private CustomerRepository repository;
     
     @GetMapping("/hellos")
     public String index() {
@@ -28,6 +35,35 @@ public class HelloController {
     	String string = restTemplate.getForObject(uri, String.class);
     	Stream.iterate(0, i->i+1).limit(10).forEach((i)->System.out.println("==============="+string +i));
     	return string;
+    }
+    
+    @GetMapping("/mongoDB")
+    public String mongoDbCalling() {
+    	repository.deleteAll();
+
+		// save a couple of customers
+		repository.save(new Customer("Alice", "Smith"));
+		repository.save(new Customer("Bob", "Smith"));
+
+		// fetch all customers
+		System.out.println("Customers found with findAll():");
+		System.out.println("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			System.out.println(customer);
+		}
+		System.out.println();
+
+		// fetch an individual customer
+		System.out.println("Customer found with findByFirstName('Alice'):");
+		System.out.println("--------------------------------");
+		System.out.println(repository.findByFirstName("Alice"));
+
+		System.out.println("Customers found with findByLastName('Smith'):");
+		System.out.println("--------------------------------");
+		for (Customer customer : repository.findByLastName("Smith")) {
+			System.out.println(customer);
+		}
+		return "done";
     }
     
 }
